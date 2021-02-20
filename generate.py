77a6243500e26f5f926aec1a7375f192a6854c38
Christian Studer <cstuder@existenz.ch>
2021-02-20 14:28:03 +0100

    WIP: Converting response into db

--- generate.py
+++ generate.py
@@ -1,5 +1,43 @@
-# Generate SQLite databases
+# Generate SQLite database
+import yaml
+import requests
+import sqlite3
+import os
 
-print("hello")
+db_filename = r"existenz-api.db"
+api_definition_file = r"existenz-api-definition.yaml"
 
-# TODO continue here
+# Delete old database
+os.remove(db_filename)
+
+# Open database
+db = sqlite3.connect(db_filename)
+c = db.cursor()
+
+# Get definitions
+with open(api_definition_file) as file:
+    definitions = yaml.safe_load(file)
+
+    for api, definition in definitions["api"].items():
+        print(f"Generating database for API {api}")
+
+        fields_string = ", ".join(
+            [f"{field['key']} {field['type']}" for field in definition["fields"]]
+        )
+
+        create_query = f"CREATE TABLE {api} ({fields_string})"
+
+        print(create_query)
+
+        # TODO run create query
+        # TODO run CREATE UNIQUE INDEX {definition['api']}_{definition['primary-key']}_index ON {definition['api']}({definition['primary-key']})
+
+        print(f"Fetching data from {definition['url']}")
+        r = requests.get(definition["url"])
+        r.raise_for_status()
+
+        # Insert rows
+        for key, row in r.json()["payload"].items():
+            values = []
+
+            # TODO collect values
